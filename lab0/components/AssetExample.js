c957bea91c582a0ea51456e2e0dc7a798d4964b1
Makarena-211 <mnfomenkov@gmail.com>
2024-09-17 22:01:26 +0300

    lab0

---
+++ lab0/components/AssetExample.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const MyApp = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+export default MyApp;
