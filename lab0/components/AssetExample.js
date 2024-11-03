c4aa2ad435bf5d80ec21de23c378b1b00c16ec07
Makarena <113354164+Makarena-211@users.noreply.github.com>
2024-11-03 15:48:49 +0300

    Update AssetExample.js

--- lab0/components/AssetExample.js
+++ lab0/components/AssetExample.js
@@ -1,9 +1,13 @@
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 const MyApp = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
       <Text>Hello World!</Text>
+      <Button
+        title="Нажми меня"
+        onPress={() => Alert.alert('Кнопка нажата!')}
+      />
     </View>
   );
 }
